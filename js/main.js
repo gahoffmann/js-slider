fd9681899b5638cfe4bee15b1bcc560ffb86f744
Giuli Hoffmann <giulihoffmann@giulis-macbook.wireless-1x.unc.edu>
2022-09-21 11:59:20 -0400

    testing

---
+++ js/main.js
@@ -0,0 +1,43 @@
+var slideIndex = 1;
+
+function plusSlides(n){
+    // console.log("inside plusSlides with a value of " + n);
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n){
+    console.log(n);
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+    // console.log("inside showSlides with a value of " + n);
+
+    var slides = document.getElementsByClassName("my-slides");
+    var dots = document.getElementsByClassName("dot");
+    // console.log(slides);
+
+    // resets to end
+    if (n<1){
+        // console.log("inside the if n<1");
+        slideIndex = slides.length;
+    }
+
+    if (n>slides.length){
+        slideIndex = 1;
+    }
+
+    // hides the slides
+    for (i=0; i<slides.length; i++){
+        slides[i].style.display = "none";
+    }
+
+    for (i=0; i<dots.length; i++){
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
+
+showSlides(slideIndex);
